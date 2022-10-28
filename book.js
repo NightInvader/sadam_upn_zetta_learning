540384d0ada4d38699d31e7781bc707d0c003238
NIghtINvader <muammarsadam13@gmail.com>
2022-10-28 18:51:39 +0700

    commit

--- book.js
+++ book.js
@@ -1,4 +1,4 @@
-function hitungHarga(buku, diskon, pajak, stok, totalOrder){
+function hitungHarga(buku, diskon, pajak, totalOrder){
     let totalBayar = 0;
     let totalHarga = 0;
     let totalStlhDiskon = 0;
@@ -12,13 +12,10 @@ function hitungHarga(buku, diskon, pajak, stok, totalOrder){
 
     for(let index =0;
         index < totalOrder; index++){
-            if(buku && buku.harga && (index < stok)){
+            if(buku && buku.harga){
                 totalHarga += buku.harga;
                 totalStlhDiskon += hargaStlhDiskon;
                 totalPajak += hargaPajak;
-            }else{
-                console.log('Stok Abis bos');
-                break;
             }
         }
 
@@ -28,18 +25,18 @@ function hitungHarga(buku, diskon, pajak, stok, totalOrder){
         console.group();
         console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
         console.log(buku.judul);
+        console.log(buku.status);
         console.log('Harga Buku \t\t: Rp.',harga);
         console.log('Jumlah Beli \t\t: ',totalOrder);
         console.log('Total Harga \t\t: Rp.',totalHarga);
         console.log('Total Diskon \t\t: Rp.',totalDiskon);
-        console.log('Harga setelah Diskon \t\t: Rp.',totalStlhDiskon);
+        console.log('Harga setelah Diskon \t: Rp.',totalStlhDiskon);
         console.log('Pajak \t\t: Rp.',totalPajak);
-        console.log('Total Pembayaran \t\t: Rp.',totalBayar);
+        console.log('Total Pembayaran \t: Rp.',totalBayar);
         console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
-        console.log('Stok yang Tersedia \t\t: Rp.',stok-totalOrder);
         console.log();
         console.groupEnd();
     }
     return totalBayar;
 }
-hitungHarga({judul:"Sea Of Thieves", harga:250000},20,10,20,2);
+hitungHarga({judul:"Sea Of Thieves", harga:250000,status:"impor"},20,10,2);
